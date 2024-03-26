9e45b4b456dc25d5de5a2822d94d27ba27c3f662
Maël Obréjan <mael.obrejan@protonmail.com>
2024-03-27 00:09:15 +0100

    refactor: explicitly use async/await

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,21 @@ import type { Plugin as VitePlugin } from "vite";
 
 type Config = {
   plugins: Array<PostHTMLPlugin<unknown>>;
-  options?: Pick<PostHTMLOptions, "parser" | "render">;
+  options?: Omit<PostHTMLOptions, "sync" | "skipParse">;
 };
 
 export default ({ plugins, options }: Config): VitePlugin => {
   return {
     name: "@malobre/vite-plugin-posthtml",
     enforce: "pre",
-    transformIndexHtml: (html) =>
-      posthtml(plugins)
+    async transformIndexHtml(html) {
+      return await posthtml(plugins)
         .process(html, {
           ...options,
           sync: false,
           skipParse: false,
         })
-        .then((r) => r.html),
+        .then(({ html }) => html);
+    },
   };
 };
